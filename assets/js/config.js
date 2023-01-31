a9c6c2023c4783e9a0df039acb3b46d968165706
Oyetola David <tolafandpro@gmail.com>
2023-01-31 17:06:48 +0100

    changes on color, logo and favicon

--- assets/js/config.js
+++ assets/js/config.js
@@ -22,8 +22,8 @@ let config = {
     body: '#f4f5fb',
     headingColor: '#566a7f',
     axisColor: '#a1acb8',
-    borderColor: '#71dd37'
+    borderColor: '#71dd37',
+    enterprise: '#00b274',
     // borderColor: '#eceef1'
-    // primary: '#696cff',
   }
 };
